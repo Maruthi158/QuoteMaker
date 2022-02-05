a02c7b6c10dfb3e6c83ea273a577af47fc4d9bc1
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2022-02-05 16:41:06 -0600

    Added the backend setup and api calls

---
+++ backend/data/quotes.js
@@ -0,0 +1,34 @@
+const quotes = [
+  {
+    _id: "1",
+    author: "Ray Bradbury",
+    quote: "You fail only if you stop writing.",
+    url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
+  },
+
+  {
+    _id: "2",
+    author: "Isaac Asimov",
+    quote:
+      "If my doctor told me I had only six minutes to live, I wouldn’t brood. I’d type a little faster.",
+    url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
+  },
+
+  {
+    _id: "3",
+    author: "Toni Morrison",
+    quote:
+      "If there’s a book that you want to read, but it hasn’t been written yet, then you must write it",
+    url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
+  },
+
+  {
+    _id: 4,
+    author: "Franz Kafka",
+    quote:
+      "Don’t bend; don’t water it down; (and) don’t try to make it logical; don’t edit your own soul according to the fashion. Rather, follow your most intense obsessions mercilessly.",
+    url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
+  },
+];
+
+module.exports = quotes;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express"); // main thing
+const quotes = require("./data/quotes");
+const dotenv = require("dotenv");
+
+const app = express(); // main thing
+dotenv.config;
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/quotes", (req, res) => {
+  res.json(quotes);
+});
+
+app.get("/api/quotes/:id", (req, res) => {
+  const quote = quotes.find((n) => n._id === req.params.id);
+
+  res.send(quote);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started running on port ${PORT}`));
