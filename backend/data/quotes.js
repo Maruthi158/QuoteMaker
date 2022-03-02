f1654a413dd35fac3bda3226267dbecc067f9320
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2022-03-01 18:52:35 -0600

    updating quotes.js

--- backend/data/quotes.js
+++ backend/data/quotes.js
@@ -3,6 +3,7 @@ const quotes = [
     _id: "1",
     author: "Ray Bradbury",
     quote: "You fail only if you stop writing.",
+    category: "Success",
     url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
   },
 
@@ -11,6 +12,7 @@ const quotes = [
     author: "Isaac Asimov",
     quote:
       "If my doctor told me I had only six minutes to live, I wouldn’t brood. I’d type a little faster.",
+    category: "Life",
     url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
   },
 
@@ -19,6 +21,7 @@ const quotes = [
     author: "Toni Morrison",
     quote:
       "If there’s a book that you want to read, but it hasn’t been written yet, then you must write it",
+    category: "Books",
     url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
   },
 
@@ -27,6 +30,7 @@ const quotes = [
     author: "Franz Kafka",
     quote:
       "Don’t bend; don’t water it down; (and) don’t try to make it logical; don’t edit your own soul according to the fashion. Rather, follow your most intense obsessions mercilessly.",
+    category: "Personality",
     url: "https://smartblogger.com/writing-quotes/#inspirational-writing-quotes",
   },
 ];
