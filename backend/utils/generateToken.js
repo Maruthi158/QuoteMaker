1b0f57856948dfbf0f3c6324a65c89dd6ede1af7
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2022-03-29 20:11:07 -0500

    Delete backend directory

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || "priyanka1234", {
-    expiresIn: "30d",
-  });
-};
-
-export default generateToken;
